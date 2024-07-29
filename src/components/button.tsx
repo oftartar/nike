6fabfa46cf22ac25a664f543caf9827523ad1da3
Ömer Faruk TARTAR <oftartar@gmail.com>
2024-07-29 16:29:53 +0300

    1-Special Offer section created

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,12 +3,20 @@ import Image from "next/image";
 const button = ({
   children,
   iconURL,
+  type,
 }: {
   children: React.ReactNode;
   iconURL?: string;
+  type?: "outline";
 }) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border border-coral-red bg-coral-red rounded-full font-montserrat text-lg leading-none text-white">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border rounded-full font-montserrat text-lg leading-none ${
+        type === "outline"
+          ? "bg-white border-slate-gray text-slate-gray"
+          : "bg-coral-red border-coral-red text-white"
+      }`}
+    >
       {children}
       {iconURL && (
         <Image src={iconURL} alt="Icon" className="ml-2 rounded-full w-5 h-5" />
--- src/sections/special-offer.tsx
+++ src/sections/special-offer.tsx
@@ -1,5 +1,41 @@
+import Image from "next/image";
+import { offer } from "../../public/images";
+import Button from "@/components/button";
+import { arrowRight } from "../../public/icons";
+
 const SpecialOffer = () => {
-  return <div>SpecialOffer</div>;
+  return (
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <Image
+          src={offer}
+          alt="Special offer"
+          width={773}
+          height={687}
+          className="object-contain w-full"
+        />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          <span className="text-coral-red">Special</span> Offer
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Embark on shopping journey that redefines your experience with
+          unbeatable deals. From premier selections to incredible savings, we
+          offer unparalleled value thar sets us apart.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Navigate a realm of possibilities designed to fullfill your unique
+          desires, surpassing loftiest expectations. Your journey with us
+          nothing short of exceptional.
+        </p>
+        <div className="mt-11 flex flex-wrap gap-5">
+          <Button iconURL={arrowRight}>Shop now</Button>
+          <Button type="outline">Learn more</Button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SpecialOffer;
